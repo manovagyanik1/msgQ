44f2e7436d06debcfe0f7aca58d9ddf41ca70f1b
shubham agrawal <manovagyanik1@gmail.com>
2017-06-27 21:53:46 +0530

    initial commit

---
+++ src/main/java/messageQueue/controllers/MainController.java
@@ -0,0 +1,17 @@
+package messageQueue.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+  @RequestMapping("/")
+  @ResponseBody
+  public String index() {
+    return "Proudly handcrafted by " + 
+        "<a href='http://messageQueue.com/en'>messageQueue</a> :)";
+  }
+
+}
